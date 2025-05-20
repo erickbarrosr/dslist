d55829812b46152fd6f1482646b005c2144f4b76
Erick Rezende <erickbarrosrezende@gmail.com>
2025-05-20 14:51:57 -0300

    feat: Buscar games por lista

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,16 +1,15 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
+
     private Long id;
     private String title;
     private Integer year;
     private String imgUrl;
     private String shortDescription;
-
-    public GameMinDTO() {
-    }
 
     public GameMinDTO(Game entity) {
         id = entity.getId();
@@ -18,6 +17,14 @@ public class GameMinDTO {
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
